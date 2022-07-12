f172819cb4a3776d2869fadb9733eb52d4838f8a
brutelearning <ejeohejidavid11@gmail.com>
2022-07-12 14:41:00 +0300

    Current Backend

--- frontend_project/settings.py
+++ frontend_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'drf_yasg',
 
 ]
-CORS_ORIGIN_WHITELIST = ('http://localhost:8000', 'http://localhost:3000')
+CORS_ORIGIN_WHITELIST = ('http://localhost:8000', 'http://localhost:3000', 'https://innoabricos.github.io')
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
@@ -137,6 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
